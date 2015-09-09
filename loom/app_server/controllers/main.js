f5e2b75f999a4a04fec06e61aea21c1973c3188b
Dylan <invision33@gmail.com>
2015-09-09 12:38:12 -0500

    add gallery route and bootstrap

--- loom/app_server/controllers/main.js
+++ loom/app_server/controllers/main.js
@@ -1,4 +1,8 @@
 /* GET home page */
 module.exports.index = function(req, res){
 	res.render('index', { title: 'Express'}); 
-}; 
+};
+
+module.exports.gallery = function(req,res){
+	res.render('gallery', {title: 'Gallery'}); 
+}
--- loom/app_server/routes/index.js
+++ loom/app_server/routes/index.js
@@ -5,4 +5,10 @@ var ctrlMain = require('../controllers/main');
 /* GET home page. */
 router.get('/', ctrlMain.index); 
 
+router.get('/gallery', ctrlMain.gallery);
+
 module.exports = router;
+
+
+
+
