a71ffc97196e33bb483e9f5ef5b07b782e75bb41
Dylan <invision33@gmail.com>
2015-09-13 17:18:57 -0500

    minor fixes with user login and gallery template

--- loom/app_server/passport/login.js
+++ loom/app_server/passport/login.js
@@ -29,7 +29,7 @@ module.exports = function(passport){
 		})
 	);
 
-	var isValidPassword = function(user, passsword){
+	var isValidPassword = function(user, password){
 		return bCrypt.compareSync(password, user.password);
 	}
 
