fbfe9ffb5ded5336051c2850c1ff944513a2af4c
Dylan <invision33@gmail.com>
2015-09-13 21:30:38 -0500

    update ace services

--- loom/public/javascripts/services/aceService.js
+++ loom/public/javascripts/services/aceService.js
@@ -13,6 +13,20 @@ loomApp.factory('ace', function($http){
 				url: 'localhost:3000/api/v1/modes',
 				cache: true 
 			}).success(callback); 
+		},
+		getTheme: function(id, callback) {
+			$http({
+				method: 'GET',
+				url: 'localhost:3000/api/v1/themes/' + id,
+				cache: true 
+			}).success(callback);
+		},
+		getModes: function(id, callback) {
+			$http({
+				method: 'GET',
+				url: 'localhost:3000/api/v1/modes/' + id,
+				cache: true
+			}).success(callback);
 		}
 	};
 }); 
