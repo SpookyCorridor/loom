005be228e767cc4fd5d409fd9dc8b2211ff061ae
Dylan <invision33@gmail.com>
2015-09-16 15:07:01 -0500

    work on adding additional editor options through modal and angular

--- loom/public/javascripts/controllers/EditorController.js
+++ loom/public/javascripts/controllers/EditorController.js
@@ -25,19 +25,53 @@ loomApp.controller('EditorController', ['$scope', 'aceFactory', '$document',func
 
 	$scope.currentTheme = 'monokai'; 
 	$scope.currentMode = 'javascript'; 
+	$scope.softTabs = false; 
+	$scope.gutter = false; 
+	$scope.fontSize = 12; 
+	$scope.tabSize = 4; 
 
 	$scope.setTheme = function() {
-		console.log($scope.currentTheme); 
+		//console.log($scope.currentTheme); 
 		app.editor.setTheme("ace/theme/" + $scope.currentTheme.name);
 	}
 
 	$scope.setMode = function() {
-		console.log($scope.currentMode); 
+		//console.log($scope.currentMode); 
 		app.editor.getSession().setMode("ace/mode/" + $scope.currentMode.name);
 	}
 
 
+	$scope.setSoftTabs = function() {
+		if ($scope.softTabs) {
+			app.editor.getSession().setUseSoftTabs(true);
+		} else {
+			app.editor.getSession().setUseSoftTabs(false);
+		}
+	} 
 
+	$scope.setGutter = function() {
+		if ($scope.gutter) {
+			app.editor.renderer.setShowGutter(true);
+		} else {
+			app.editor.renderer.setShowGutter(false);
+		}
+	}
+
+	
+	$scope.increaseFont = function() {
+		$scope.fontSize += 2; 
+		document.getElementById('editor').style.fontSize= $scope.fontSize + '';
+	}
+
+	$scope.decreaseFont = function() {
+		$scope.fontSize -= 2; 
+		document.getElementById('editor').style.fontSize= $scope.fontSize + '';
+	}
+
+	$scope.setTabSize = function() {
+		console.log('changing tab size'); 
+		app.editor.getSession().setTabSize($scope.tabSize);
+	}
 
 
 }])
